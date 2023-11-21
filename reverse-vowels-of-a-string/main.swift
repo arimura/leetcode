4080a364f0bd042eda5eaccfd816c33a67ae38fb
arimura <33759663+arimura@users.noreply.github.com>
2023-11-22 06:57:40 +0900

    Format

--- reverse-vowels-of-a-string/main.swift
+++ reverse-vowels-of-a-string/main.swift
@@ -1,58 +1,58 @@
 func reverseVowels(_ s: String) -> String {
-    var sArray = Array(s)
-    let len = sArray.count
-    var startIdx = 0
-    var endIdx = len - 1
-    var isEnd = false
+  var sArray = Array(s)
+  let len = sArray.count
+  var startIdx = 0
+  var endIdx = len - 1
+  var isEnd = false
+  while true {
     while true {
-        while true {
-            if (startIdx == endIdx){
-                isEnd = true
-                break
-            }
-            
-            if (isVowels(sArray[startIdx])){
-                break
-            }
-            startIdx += 1
-        }
-        
-        while true {
-            if (startIdx == endIdx){
-                isEnd = true
-                break
-            }
-            if(isVowels(sArray[endIdx])){
-                break
-            }
-            endIdx -= 1
-        }
-        
-        if(!isEnd){
-            let tmp = sArray[endIdx]
-            sArray[endIdx] = sArray[startIdx]
-            sArray[startIdx] = tmp
-        }else {
-            break
-        }
-        startIdx += 1
-        endIdx += -1
-        if(endIdx <= startIdx ){
-            break
-        }
-        
+      if startIdx == endIdx {
+        isEnd = true
+        break
+      }
+
+      if isVowels(sArray[startIdx]) {
+        break
+      }
+      startIdx += 1
     }
-    
-    return String(sArray)
+
+    while true {
+      if startIdx == endIdx {
+        isEnd = true
+        break
+      }
+      if isVowels(sArray[endIdx]) {
+        break
+      }
+      endIdx -= 1
+    }
+
+    if !isEnd {
+      let tmp = sArray[endIdx]
+      sArray[endIdx] = sArray[startIdx]
+      sArray[startIdx] = tmp
+    } else {
+      break
+    }
+    startIdx += 1
+    endIdx += -1
+    if endIdx <= startIdx {
+      break
+    }
+
+  }
+
+  return String(sArray)
 }
 
 func isVowels(_ c: Character) -> Bool {
-    switch c {
-    case "a", "A", "e", "E", "i", "I", "o", "O", "u", "U":
-        return true
-    default:
-        return false
-    }
+  switch c {
+  case "a", "A", "e", "E", "i", "I", "o", "O", "u", "U":
+    return true
+  default:
+    return false
+  }
 }
 
-print(reverseVowels("leetcode"));
+print(reverseVowels("leetcode"))
