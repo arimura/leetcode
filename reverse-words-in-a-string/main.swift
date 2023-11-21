5fcf83a1255be31673220b090e6a3fe2eedbddbd
arimura <33759663+arimura@users.noreply.github.com>
2023-11-22 06:58:35 +0900

    Format

--- reverse-words-in-a-string/main.swift
+++ reverse-words-in-a-string/main.swift
@@ -1,33 +1,33 @@
 func reverseWords(_ s: String) -> String {
-    let array = Array(s)
-    var reversed : [String] = []
-    var word:[Character] = []
-    for c in array {
-        if(c == " "){
-            if (word.count == 0){
-                //new word
-                continue
-            }else{
-                //splitter
-                reversed.insert(String(word), at:0)
-                word = []
-            }
-        }else{
-            word.append(c)
-        }
-    }
-    if (word.count != 0){
-        reversed.insert(String(word), at:0)
+  let array = Array(s)
+  var reversed: [String] = []
+  var word: [Character] = []
+  for c in array {
+    if c == " " {
+      if word.count == 0 {
+        //new word
+        continue
+      } else {
+        //splitter
+        reversed.insert(String(word), at: 0)
         word = []
+      }
+    } else {
+      word.append(c)
     }
-    
-    var s: String = ""
-    for w in reversed {
-        s.append(w)
-        s.append(" ")
-    }
-    s.removeLast()
-    return s
+  }
+  if word.count != 0 {
+    reversed.insert(String(word), at: 0)
+    word = []
+  }
+
+  var s: String = ""
+  for w in reversed {
+    s.append(w)
+    s.append(" ")
+  }
+  s.removeLast()
+  return s
 }
 
 print(reverseWords("a good   example"))
