70221e8b9e695b82a41ec9ac3d4aac21a414b53f
arimura <33759663+arimura@users.noreply.github.com>
2023-12-11 21:19:09 +0900

    Print node

--- maximum-depth-of-binary-tree/main.swift
+++ maximum-depth-of-binary-tree/main.swift
@@ -17,22 +17,51 @@ public class TreeNode {
     self.left = left
     self.right = right
   }
-}
 
-func treeNode(fromArray array: [Int]) -> TreeNode {
-  var head = TreeNode()
-  var n = head
-  var isLeft = true
-  for e in array {
-    if isLeft {
-      if e != nil {
-        n.left = TreeNode(e)
-      }
-    }else{
-      if e != nil {
-        n.right = TreeNode(e)
-      }
-      n = n.left
+  func toString() -> String {
+    return buildTreeString(isTail: true)
+  }
+
+  private func buildTreeString(prefix: String = "", isTail: Bool) -> String {
+    var result = ""
+
+    if right != nil {
+      let newPrefix = prefix + (isTail ? "│   " : "    ")
+      result += right!.buildTreeString(prefix: newPrefix, isTail: false)
     }
+
+    result += prefix + (isTail ? "└── " : "┌── ") + String(val) + "\n"
+
+    if left != nil {
+      let newPrefix = prefix + (isTail ? "    " : "│   ")
+      result += left!.buildTreeString(prefix: newPrefix, isTail: true)
+    }
+
+    return result
   }
 }
+
+var n1 = TreeNode(1)
+var n2 = TreeNode(2)
+var n3 = TreeNode(3)
+n1.left = n2
+n1.right = n3
+print(n1.toString())
+
+// func treeNode(fromArray array: [Int]) -> TreeNode {
+//   var head = TreeNode()
+//   var n = head
+//   var isLeft = true
+//   for e in array {
+//     if isLeft {
+//       if e != nil {
+//         n.left = TreeNode(e)
+//       }
+//     }else{
+//       if e != nil {
+//         n.right = TreeNode(e)
+//       }
+//       n = n.left
+//     }
+//   }
+// }
