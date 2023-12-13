f02d3e99559e7a566790d7a5842629b1ca4eff12
arimura <33759663+arimura@users.noreply.github.com>
2023-12-13 10:44:45 +0900

    Add treeNode

--- swift-util/util.swift
+++ swift-util/util.swift
@@ -1,3 +1,43 @@
-public func hoge(){
-    print("hoge")
+public class TreeNode {
+  public var val: Int
+  public var left: TreeNode?
+  public var right: TreeNode?
+  public init() {
+    self.val = 0
+    self.left = nil
+    self.right = nil
+  }
+  public init(_ val: Int) {
+    self.val = val
+    self.left = nil
+    self.right = nil
+  }
+  public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+    self.val = val
+    self.left = left
+    self.right = right
+  }
+
+  func toString() -> String {
+    return buildTreeString(isTail: true)
+  }
+
+  private func buildTreeString(prefix: String = "", isTail: Bool) -> String {
+    var result = ""
+
+    if right != nil {
+      let newPrefix = prefix + (isTail ? "│   " : "    ")
+      result += right!.buildTreeString(prefix: newPrefix, isTail: false)
+    }
+
+    result += prefix + (isTail ? "└── " : "┌── ") + String(val) + "\n"
+
+    if left != nil {
+      let newPrefix = prefix + (isTail ? "    " : "│   ")
+      result += left!.buildTreeString(prefix: newPrefix, isTail: true)
+    }
+
+    return result
+  }
 }
+
