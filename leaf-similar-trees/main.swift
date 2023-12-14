bb583d8f1a987747d0a48e35f12deed19c6843f0
arimura <33759663+arimura@users.noreply.github.com>
2023-12-14 21:42:06 +0900

    Add test case

---
+++ leaf-similar-trees/main.swift
@@ -0,0 +1,17 @@
+// class Solution {
+//     func leafSimilar(_ root1: TreeNode?, _ root2: TreeNode?) -> Bool {
+        
+//     }
+// }
+
+
+//case1
+let root1 = TreeNode(5,
+    TreeNode(6),
+    TreeNode(2, TreeNode(7), TreeNode(4))
+)
+
+print(root1.toString())
+
+
+
