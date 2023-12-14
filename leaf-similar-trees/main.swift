a4225df3d35cccd6cca3ce503e19bd06e0cba27c
arimura <33759663+arimura@users.noreply.github.com>
2023-12-14 22:28:30 +0900

    Add answear

--- leaf-similar-trees/main.swift
+++ leaf-similar-trees/main.swift
@@ -1,17 +1,45 @@
-// class Solution {
-//     func leafSimilar(_ root1: TreeNode?, _ root2: TreeNode?) -> Bool {
-        
-//     }
-// }
+class Solution {
+    func leafSimilar(_ root1: TreeNode?, _ root2: TreeNode?) -> Bool {
+        return leafArray(root1) == leafArray(root2)
+    }
 
+    func leafArray(_ t: TreeNode?) -> Array<Int>{
+        var array: Array<Int> = []
+        leafArrayTrv(t, &array)
+        return array
+    }
+
+    func leafArrayTrv(_ t: TreeNode?, _ a: inout Array<Int>) {
+        if t!.left != nil {
+            leafArrayTrv(t!.left, &a)
+        }
+        if t!.right != nil {
+            leafArrayTrv(t!.right, &a)
+        } 
+        if t!.right == nil && t!.left == nil {
+            a.append(t!.val)
+        }
+    }
+}
 
 //case1
-let root1 = TreeNode(5,
-    TreeNode(6),
-    TreeNode(2, TreeNode(7), TreeNode(4))
-)
+typealias N = TreeNode
 
-print(root1.toString())
+let root1 =
+  N(
+    3,
+    N(
+      5,
+      N(6),
+      N(
+        2,
+        N(7),
+        N(4))
+    ),
+    N(1, N(9), N(8))
+  )
+let root2 = 
+ N(3,N(5,N(6),N(7)),N(1,N(4),N(2,N(9),N(8))))
 
-
-
+let s = Solution()
+s.leafSimilar(root1, root2)
