85b87621b407ff9709a2427ed79d0993498a6c6e
arimura <33759663+arimura@users.noreply.github.com>
2023-12-19 23:54:49 +0900

    Fix

--- binary-tree-right-side-view/main.swift
+++ binary-tree-right-side-view/main.swift
@@ -1,29 +1,37 @@
 class Solution {
-    var rightVals : Array<Int> = []
-    func rightSideView(_ root: TreeNode?) -> [Int] {
-        if root == nil {
-            return []
-        }
-        traverseRight(root)
-        return rightVals
+  var vals: [Int] = []
+  func rightSideView(_ root: TreeNode?) -> [Int] {
+    if root == nil {
+      return []
+    }
+    traverse(root, 1)
+    return vals
+  }
+
+  func traverse(_ n: TreeNode?, _ depth: Int) {
+    if vals.count < depth {
+      vals.append(n!.val)
     }
 
-    func traverseRight(_ n: TreeNode?) {
-        rightVals.append(n!.val)
-        if n!.right != nil {
-            traverseRight(n!.right)
-        }
+    if n!.right != nil {
+      traverse(n!.right, depth + 1)
     }
+
+    if n!.left != nil {
+      traverse(n!.left, depth + 1)
+    }
+  }
 }
 
 let cases = [
-([1,2,3,nil,5,nil,4], [1,3,4]),
-([1,nil,3], [1,3]),
-([], []),
+  ([1, 2, 3, nil, 5, nil, 4], [1, 3, 4]),
+  ([1, nil, 3], [1, 3]),
+  ([], []),
+  ([1, 2], [1, 2]),
 ]
 
 for c in cases {
-    let s = Solution()
-    let r = s.rightSideView(treeNode(c.0))
-    assert(c.1 == r, "c: \(c), r: \(r)")
+  let s = Solution()
+  let r = s.rightSideView(treeNode(c.0))
+  assert(c.1 == r, "c: \(c), r: \(r)")
 }
