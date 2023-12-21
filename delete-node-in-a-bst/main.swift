89264c61b4d5086dd22d617e417a07a520088a5b
arimura <33759663+arimura@users.noreply.github.com>
2023-12-21 19:54:05 +0900

    Add proto

--- delete-node-in-a-bst/main.swift
+++ delete-node-in-a-bst/main.swift
@@ -1,5 +1,51 @@
+class Solution {
+  func deleteNode(_ root: TreeNode?, _ key: Int) -> TreeNode? {
+    if root == nil {
+      return nil
+    }
+
+    var node = root
+    var prev = root
+    while let currentNode = node, let currentPrev = prev {
+        print(currentNode.val)
+      if currentNode.val == key {
+        swap(node: currentNode, prev: currentPrev)
+        return root
+      }
+      if key < currentNode.val {
+        node = currentNode.left
+        prev = currentNode
+      } else {
+        node = currentNode.right
+        prev = currentNode
+      }
+    }
+    return root
+  }
+
+  func swap(node: TreeNode, prev: TreeNode) {
+    var pad: TreeNode?
+    if node.left != nil {
+      pad = node.left
+    } else {
+      pad = node.right
+    }
+
+    if prev.left!.val == node.val {
+      prev.left = pad
+    } else {
+      prev.right = pad
+    }
+  }
+}
+
 let cases = [
   ([5, 3, 6, 2, 4, nil, 7], 3, [5, 4, 6, 2, nil, nil, 7]),
   ([5, 3, 6, 2, 4, nil, 7], 0, [5, 3, 6, 2, 4, nil, 7]),
   ([], 0, []),
 ]
+
+for c in cases {
+    let s = Solution().deleteNode(treeNode(c.0), c.1)
+    print(s?.toString() ?? "Node")
+}
