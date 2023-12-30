50dbb6f22eea3c47e891dfd5bb03f95dd143d014
arimura <33759663+arimura@users.noreply.github.com>
2023-12-30 18:49:32 +0900

    Prototype

--- number-of-provinces/main.swift
+++ number-of-provinces/main.swift
@@ -1,6 +1,38 @@
 class Solution {
+  var visited: [[Bool]]?
+  var rowL: Int?
+  var colL: Int?
+  var count: Int?
   func findCircleNum(_ isConnected: [[Int]]) -> Int {
+    rowL =  isConnected.count
+    colL =  isConnected[0].count
+    count = colL * rowL
+    visited = Array(repeating:Array(repeating: false, count:colL), count: rowL)
+  }
 
+  func traverse(_ row: Int, _ col: Int){
+    if visited[row][col] {
+        return
+    }
+    visited[row][col] = true
+    count -= 1
+    
+    //go left
+    if col != 0 {
+        traverse(row, col - 1)
+    }
+    //go right
+    if col < colL - 1 {
+        traverse(row, col + 1)
+    }
+    //go up
+    if row != 0 {
+        traverse(row + 1, col)
+    }
+    //go down
+    if row < rowL - 1 {
+        traverse(row - 1, col)
+    }
   }
 }
 
@@ -8,6 +40,7 @@ let cases = [
   ([[1, 1, 0], [1, 1, 0], [0, 0, 1]], 2),
   ([[1, 0, 0], [0, 1, 0], [0, 0, 1]], 3),
 ]
+
 for c in cases {
   let s = Solution()
   let r = s.findCircleNum(c.0)
