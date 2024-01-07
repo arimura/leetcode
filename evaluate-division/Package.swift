44dc9e97b7801eca8ad1bfc5e037a294f9ac345f
arimura <33759663+arimura@users.noreply.github.com>
2024-01-07 16:25:25 +0900

    User swiftpm

---
+++ evaluate-division/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "evaluate-division",
+  dependencies: [
+    // Local package dependency
+    .package(path: "../LeetcodeUtil")  // Relative or absolute path
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "evaluate-division",
+      dependencies: [
+        "LeetcodeUtil"
+      ])
+  ]
+)
