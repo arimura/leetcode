593a39adfbd82c86efab1c5dc63bc70f9571186e
arimura <33759663+arimura@users.noreply.github.com>
2024-01-14 16:30:13 +0900

    Add template

--- nearest-exit-from-entrance-in-maze/go/main.go
+++ nearest-exit-from-entrance-in-maze/go/main.go
@@ -1,12 +1,22 @@
 package main
 
+import "fmt"
+
 func nearestExit(maze [][]byte, entrance []int) int {
 
 	return 0
 }
 
 func main() {
+	cases := []Case{}
+
+	for _, c := range cases {
+		fmt.Println(c)
+	}
 }
+
+//byte = 8-bit unsigned integer
+//1 1 1 1 1 1 1 1 = 1 byte = 2^8 = 256
 
 type Case struct {
 	maze     [][]byte
