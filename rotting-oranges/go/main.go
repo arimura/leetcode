0443e8ea6574aeb5a7ea2c3125aee179a67e1bbe
arimura <33759663+arimura@users.noreply.github.com>
2024-01-18 09:01:41 +0900

    Add template

--- rotting-oranges/go/main.go
+++ rotting-oranges/go/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
 func orangesRotting(grid [][]int) int {
 
+}
+
+func main() {
+	cases := []Case{
+		{
+			[][]int{{2, 1, 1}, {1, 1, 0}, {0, 1, 1}},
+			4,
+		},
+	}
+	for _, c := range cases {
+		r := orangesRotting(c.grid)
+		if r != c.output {
+			panic(fmt.Sprintf("c: %+v, r: %d", c, r))
+		}
+	}
+}
+
+type Case struct {
+	grid   [][]int
+	output int
 }
