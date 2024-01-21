b9ccf9e296304692fbe57fcb679846640061efaf
arimura <33759663+arimura@users.noreply.github.com>
2024-01-21 22:39:31 +0900

    Add

--- kth-largest-element-in-an-array/main.go
+++ kth-largest-element-in-an-array/main.go
@@ -1,6 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Item struct {
+	value    string
+	priority int
+	index    int
+}
+
+type ProrityQueue []*Item
+
+func (pq ProrityQueue) Len() int { return len(pq) }
+func (pq ProrityQueue) Less(i, j int) bool {
+	return pq[i].priority > pq[j].priority
+}
+func (pq ProrityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
 
 func findKthLargest(nums []int, k int) int {
 
