9bb4aaaa68ab4883dee9d7dc21d7b1fc00705fd2
arimura <33759663+arimura@users.noreply.github.com>
2024-01-26 13:18:42 +0900

    Add answear

--- n-th-tribonacci-number/go/main.go
+++ n-th-tribonacci-number/go/main.go
@@ -3,7 +3,23 @@ package main
 import "fmt"
 
 func tribonacci(n int) int {
-
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 1
+	}
+	nums := make([]int, n+1)
+	nums[0] = 0
+	nums[1] = 1
+	nums[2] = 1
+	for i := 3; i <= n; i++ {
+		nums[i] = nums[i-3] + nums[i-2] + nums[i-1]
+	}
+	return nums[n]
 }
 
 func main() {
