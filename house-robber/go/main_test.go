54f1f59ada65541ee10e0874f1ff3efceaa8dc6b
arimura <33759663+arimura@users.noreply.github.com>
2024-01-27 10:58:40 +0900

    Add benchmark

--- house-robber/go/main_test.go
+++ house-robber/go/main_test.go
@@ -8,3 +8,30 @@ func TestBase(t *testing.T) {
 		t.Errorf("i: %+v", i)
 	}
 }
+
+func BenchmarkRob10(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		rob(makeNums(10))
+	}
+}
+
+func BenchmarkRob20(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		rob(makeNums(20))
+	}
+}
+
+func BenchmarkRob30(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		rob(makeNums(30))
+	}
+}
+
+func makeNums(n int) []int {
+	nums := make([]int, n)
+	for i := range nums {
+		nums[i] = i % 10
+	}
+	return nums
+
+}
