f39916a157b409ac7bac7f94652e6c61bf3f9cde
arimura <33759663+arimura@users.noreply.github.com>
2024-01-28 10:47:59 +0900

    Add answear

--- house-robber/go/main.go
+++ house-robber/go/main.go
@@ -19,7 +19,13 @@ func rob(nums []int) int {
 
 func rec(i int, prevRob bool, nums []int) int {
 	if prevRob {
-		return rec(i+1, false, nums)
+		m := noRobMemo[i]
+		if m != -1 {
+			return m
+		}
+		r := rec(i+1, false, nums)
+		noRobMemo[i] = r
+		return r
 	}
 	if len(nums) <= i {
 		return 0
@@ -27,9 +33,16 @@ func rec(i int, prevRob bool, nums []int) int {
 	if len(nums)-1 == i {
 		return nums[i]
 	}
-
-	r := nums[i] + rec(i+1, true, nums)
-	nr := rec(i+1, false, nums)
+	r := robMemo[i]
+	if r == -1 {
+		r = nums[i] + rec(i+1, true, nums)
+		robMemo[i] = r
+	}
+	nr := noRobMemo[i]
+	if nr == -1 {
+		nr = rec(i+1, false, nums)
+		noRobMemo[i] = nr
+	}
 	if r < nr {
 		return nr
 	} else {
