2dacdcc7e672034d2cbac7289a3af1ad4a1cb850
arimura <33759663+arimura@users.noreply.github.com>
2024-02-04 00:49:16 +0900

    Add answear

--- house-robber/swift/main.swift
+++ house-robber/swift/main.swift
@@ -1,6 +1,13 @@
 class Solution {
+    //[i][0=rob, 1=no rob] 
+    private var dp :[[Int]]!
     func rob(_ nums: [Int]) -> Int {
-        
+        dp = Array(repeating: Array(repeating: 0, count: 2), count: nums.count + 1)
+        for i in (0..<nums.count).reversed() {
+            dp[i][0] = dp[i+1][1] + nums[i]
+            dp[i][1] = dp[i+1][0] < dp[i+1][1] ? dp[i+1][1] : dp[i+1][0]
+        } 
+        return dp[0][0] < dp[0][1] ? dp[0][1] : dp[0][0]
     }
 }
 
