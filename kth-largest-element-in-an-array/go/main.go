d53c9ec0975dc03146ae67c6e078e8513d2b7874
arimura <33759663+arimura@users.noreply.github.com>
2024-02-11 23:18:06 +0900

    Tmp

--- kth-largest-element-in-an-array/go/main.go
+++ kth-largest-element-in-an-array/go/main.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+var heap = make([]int, 100000)
+var size = 0
+
+func findKthLargest(nums []int, k int) int {
+	for _, n := range nums {
+		push(n)
+	}
+
+	fmt.Println(heap[:100])
+	return 0
+}
+
+func push(num int) {
+	size++
+
+	for size > 0 {
+		p := (size - 1) / 2
+		if heap[p] <= num {
+			break
+		}
+		heap[size] = heap[p]
+		size = p
+	}
+	heap[size] = num
+}
+
 func main() {
 	cases := []Case{
 		{
