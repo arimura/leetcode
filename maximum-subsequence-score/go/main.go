a8b3afbce1465d9a3e65b7d61f5a178dab4b3e2f
arimura <33759663+arimura@users.noreply.github.com>
2024-03-04 09:43:43 +0900

    Add template

---
+++ maximum-subsequence-score/go/main.go
@@ -0,0 +1,30 @@
+package main
+
+func maxScore(nums1 []int, nums2 []int, k int) int64 {
+
+	return 0
+}
+
+type c struct {
+	num1 []int
+	num2 []int
+	k    int
+}
+
+func main() {
+	cases := []c{
+		{
+			[]int{1, 3, 3, 2},
+			[]int{2, 1, 3, 4},
+			3,
+		},
+		{
+			[]int{4, 2, 3, 1, 1},
+			[]int{7, 5, 10, 9, 6},
+			1,
+		},
+	}
+	for _, c := range cases {
+
+	}
+}
