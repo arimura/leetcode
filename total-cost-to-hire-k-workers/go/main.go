7b77a33adb351d33efec0b579452e62cc83ff26a
arimura <33759663+arimura@users.noreply.github.com>
2024-03-10 12:48:52 +0900

    tmp

--- total-cost-to-hire-k-workers/go/main.go
+++ total-cost-to-hire-k-workers/go/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type MinHeap [][]int
 
 func (m MinHeap) Len() int {
@@ -27,8 +29,41 @@ func (m *MinHeap) Pop() any {
 }
 
 func totalCost(costs []int, k int, candidates int) int64 {
+	leftI := 0
+	rightI := len(costs)
+
+	for sessionI := 0; sessionI < k; sessionI++ {
+
+	}
 
 }
 
 func main() {
+	cases := []testCase{
+		{
+			[]int{17, 12, 10, 2, 7, 2, 11, 20, 8},
+			3,
+			4,
+			11,
+		},
+		{
+			[]int{1, 2, 4, 1},
+			3,
+			3,
+			4,
+		},
+	}
+	for _, c := range cases {
+		r := totalCost(c.costs, c.k, c.candidates)
+		if r != c.out {
+			panic(fmt.Sprintf("c: %+v, r: %d", c, r))
+		}
+	}
+}
+
+type testCase struct {
+	costs      []int
+	k          int
+	candidates int
+	out        int64
 }
