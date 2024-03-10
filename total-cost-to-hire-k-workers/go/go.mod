661660e69b3fb87f70ff218da01052d2aabbdab2
arimura <33759663+arimura@users.noreply.github.com>
2024-03-10 09:27:02 +0900

    Add template

---
+++ total-cost-to-hire-k-workers/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/arimura/leetcode/total-cost-to-hire-k-workers
+
+go 1.21.6
---
+++ total-cost-to-hire-k-workers/go/main.go
@@ -0,0 +1,8 @@
+package main
+
+func totalCost(costs []int, k int, candidates int) int64 {
+
+}
+
+func main() {
+}
