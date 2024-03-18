3b7e216b6c2fae6b32c7c8430a825350292b4012
arimura <33759663+arimura@users.noreply.github.com>
2024-03-18 09:26:47 +0900

    add template

--- guess-number-higher-or-lower/go/main.go
+++ guess-number-higher-or-lower/go/main.go
@@ -1,9 +1,36 @@
 package main
 
+var pick int
+
 func guessNumber(n int) int {
 
 }
 
-func main() {
+type TestCase struct {
+	n    int
+	pick int
+}
 
+func main() {
+	cases := []TestCase{
+		{
+			n:    10,
+			pick: 6,
+		},
+		{
+			n:    1,
+			pick: 1,
+		},
+		{
+			n:    2,
+			pick: 1,
+		},
+	}
+	for _, c := range cases {
+		pick = c.pick
+		r := guessNumber(c.n)
+		if r != c.pick {
+			panic(c)
+		}
+	}
 }
