5ea7f2adeec60e66c0c9fe2b8568b9c22dd10f01
arimura <33759663+arimura@users.noreply.github.com>
2024-03-20 23:24:12 +0900

    Add tests

--- successful-pairs-of-spells-and-potions/go/main.go
+++ successful-pairs-of-spells-and-potions/go/main.go
@@ -3,3 +3,26 @@ func successfulPairs(spells []int, potions []int, success int64) []int {
 
 }
 
+type TestCase struct {
+	spells  []int
+	potions []int
+	success int
+	output  []int
+}
+
+func main(){
+	cases := []TestCase{
+		{
+			spells: {5,1,3},
+			potions: {1,2,3,4,5},
+			success: 7,
+			output: {4,0,3}
+		}
+	}
+	for _, c := range cases {
+		r := successfulPairs(c.spells, c.potions, c.success)
+		if r != c.output {
+			panic(fmt.Sprintf("c: %+v, r: %+v", c, r)
+		}
+	}
+}
