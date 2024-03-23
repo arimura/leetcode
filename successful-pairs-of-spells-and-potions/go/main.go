4572926cc2218304a8dea465f163e65df51ddb44
arimura <33759663+arimura@users.noreply.github.com>
2024-03-23 23:04:24 +0900

    Tmp

--- successful-pairs-of-spells-and-potions/go/main.go
+++ successful-pairs-of-spells-and-potions/go/main.go
@@ -1,7 +1,14 @@
 
 func successfulPairs(spells []int, potions []int, success int64) []int {
-
+	r := make([]int, len(spells))
+	for _, spell := range spells {
+		sorted := sort.Slice(portions, func(i, j int) bool {
+			return i < j
+		})
+	}
 }
+
+// func findMin
 
 type TestCase struct {
 	spells  []int
