e15de2833ce2e970d53cfa241cc587fa49430c7f
arimura <33759663+arimura@users.noreply.github.com>
2024-03-31 22:02:29 +0900

    Add java template

---
+++ koko-eating-bananas/java/Main.java
@@ -0,0 +1,13 @@
+class Main {
+    public int minEatingSpeed(int[] piles, int h) {
+        
+    }
+
+    public static void main(String[] args) {
+    }
+
+    static class TestCase {
+        int[] piles;
+        int h;
+    }
+}
