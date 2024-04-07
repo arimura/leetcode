caa7c721f060c783d2a78624277fcde9e5883a72
arimura <33759663+arimura@users.noreply.github.com>
2024-04-07 22:52:01 +0900

    Tmp

--- combination-sum-iii/javascript/main.js
+++ combination-sum-iii/javascript/main.js
@@ -4,16 +4,28 @@
  * @return {number[][]}
  */
 var combinationSum3 = function(k, n) {
-   return  
+   find([],0, k, 1,n);
+   return r;
 };
 
-const find = function(k, n, nums) {
-   for(const i = 0; i < nums.length; i++){
-      
+let r = [];
+
+const find = function(combi, sum, k, idx, n) {
+   console.log("idx is " + idx);
+   if(combi.length == k) {
+      console.log(combi);
+      if(sum == n){
+         r.push(combi);
+      }
+      return ;
    }
+
+   combi.push(idx);
+
+   for (i = idx + 1; i <=9; i++){
+      const c = combi.slice();
+      find(c, sum + idx, k, i, n);
+   }   
 }
 
-
-
-
-console.log();
+console.log(combinationSum3(3,9));
