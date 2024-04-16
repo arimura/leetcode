5c12a8ba7e7e1fa91cd6d61c475af1ef68b57746
arimura <33759663+arimura@users.noreply.github.com>
2024-04-16 22:52:58 +0900

    Build trie

--- search-suggestions-system/main.js
+++ search-suggestions-system/main.js
@@ -1,16 +1,34 @@
 const assert = require('assert');
 
 class Node {
-    constructor(){
-        this.children = [];
+    constructor() {
+        this.char = "";
+        this.children = {};
         this.isEnd = false;
     }
 
-    /**
-     * @param {string[]} words
-     */
-    load(words){
-        
+    loadWords(words) {
+        for (const word of words) {
+            const c = word[0];
+            if(!this.children[c]){
+                this.children[c] = new Node();
+            }
+            this.children[c].load(word);
+        }
+    }
+
+    load(word) {
+        this.char = word[0];
+        if (word.length === 1) {
+            this.isEnd = true;
+            return;
+        }
+        let nc = word[1];
+        if (!this.children[nc]) {
+            let c = new Node();
+            this.children[nc] = c;
+        }
+        this.children[nc].load(word.slice(1));
     }
 }
 
@@ -19,12 +37,16 @@ class Node {
  * @param {string} searchWord
  * @return {string[][]}
  */
-var suggestedProducts = function(products, searchWord) {
+var suggestedProducts = function (products, searchWord) {
 
 
     return [];
 };
 
-assert.strictEqual(suggestedProducts(["mobile","mouse","moneypot","monitor","mousepad"],  "mouse"),[]);
+let n = new Node();
+n.loadWords(["mobile", "mouse", "moneypot", "monitor", "mousepad"]);
+
+
+// assert.strictEqual(suggestedProducts(["mobile", "mouse", "moneypot", "monitor", "mousepad"], "mouse"), []);
 
 
