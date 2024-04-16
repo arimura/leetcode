c1f01b897c2ff5f0a7bf322c7a771ea705917bcf
arimura <33759663+arimura@users.noreply.github.com>
2024-04-16 20:11:44 +0900

    Template

---
+++ search-suggestions-system/main.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+
+class Node {
+    constructor(){
+        this.children = [];
+        this.isEnd = false;
+    }
+
+    /**
+     * @param {string[]} words
+     */
+    load(words){
+        
+    }
+}
+
+/**
+ * @param {string[]} products
+ * @param {string} searchWord
+ * @return {string[][]}
+ */
+var suggestedProducts = function(products, searchWord) {
+
+
+    return [];
+};
+
+assert.strictEqual(suggestedProducts(["mobile","mouse","moneypot","monitor","mousepad"],  "mouse"),[]);
+
+
