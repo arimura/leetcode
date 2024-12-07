20634f15422c94135fe620fd8ad3c47f2283c75f
arimura <33759663+arimura@users.noreply.github.com>
2024-12-07 22:17:01 +0900

    Add dat build

--- implement-trie-prefix-tree/go/main.go
+++ implement-trie-prefix-tree/go/main.go
@@ -36,3 +36,28 @@ type dartsBuild struct {
 	nextCheckPos int
 	err          int
 }
+
+func Build(key [][]rune, freq []int) Darts {
+	var d = new(dartsBuild)
+
+	d.key = key
+	d.freq = freq
+	d.resize(512)
+
+	d.darts.Base[0] = 1
+	d.nextCheckPos = 0
+
+	var rootNode node
+	rootNode.depth = 0
+	rootNode.left = 0
+	rootNode.right = len(key)
+
+	siblings := d.fetch(rootNode)
+	d.insert(siblings)
+
+	if d.err < 0 {
+		panic("Build error")
+	}
+
+	return d.darts
+}
