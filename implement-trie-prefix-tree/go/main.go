14368589c37d341e44c858c60309599c7b5ac605
arimura <33759663+arimura@users.noreply.github.com>
2024-12-08 21:36:01 +0900

    Imple fetch

--- implement-trie-prefix-tree/go/main.go
+++ implement-trie-prefix-tree/go/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 type node struct {
 	code               rune
 	depth, left, right int
@@ -60,4 +64,62 @@ func Build(key [][]rune, freq []int) Darts {
 	}
 
 	return d.darts
+}
+
+func (d *dartsBuild) resize(newSize int) {
+	if newSize > cap(d.darts.Base) {
+		d.darts.Base = append(d.darts.Base, make([]int, (newSize-len(d.darts.Base)))...)
+		d.darts.Check = append(d.darts.Check, make([]int, (newSize-len(d.darts.Check)))...)
+		d.used = append(d.used, make([]bool, (newSize-len(d.used)))...)
+	} else {
+		d.darts.Base = d.darts.Base[:newSize]
+		d.darts.Check = d.darts.Check[:newSize]
+		d.used = d.used[:newSize]
+	}
+}
+
+func (d *dartsBuild) fetch(parent node) []node {
+	var siblings = make([]node, 0, 2)
+	if d.err < 0 {
+		return siblings[0:0]
+	}
+	var prev rune = 0
+
+	for i := parent.left; i < parent.right; i++ {
+		if len(d.key[i]) < parent.depth {
+			continue
+		}
+
+		tmp := d.key[i]
+
+		var cur rune = 0
+		if len(d.key[i]) != parent.depth {
+			cur = tmp[parent.depth] + 1
+		}
+		if prev > cur {
+			fmt.Println(prev, cur, i, parent.depth, d.key[i])
+			fmt.Println(d.key[i])
+			panic("fetch error 1")
+			d.err = -3
+			return siblings[0:0]
+		}
+
+		if cur != prev || len(siblings) == 0 {
+			var tmpNode node
+			tmpNode.depth = parallel.depth + 1
+			tmpNode.code = cur
+			tmpNode.left = i
+			if len(siblings) != 0 {
+				siblings[len(siblings)-1].right = i
+			}
+			siblings = append(siblings, tmpNode)
+		}
+
+		prev = cur
+	}
+
+	if len(siblings) != 0 {
+		siblings[len(siblings)-1].right = parent.right
+	}
+	return siblings
 }
