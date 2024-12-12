748d1d197633b0de2e96b0c9a90654d367322685
arimura <33759663+arimura@users.noreply.github.com>
2024-12-12 20:20:29 +0900

    add test

--- double-array-trie/go/tree/trie_test.go
+++ double-array-trie/go/tree/trie_test.go
@@ -7,18 +7,24 @@ import (
 func TestRelocate(t *testing.T) {
 	tr := New(30)
 	tr.insert(1, 1, 'p', 2)
+	tr.insert(2, 2, 'o', 3)
 
-	r, n := tr.walk(1, 'p')
-	if !r {
-		t.Fatalf("walk failed. s: %d, c: %c", 1, 'p')
-	}
-	if n != 2 {
-		t.Fatalf("walk failed. s: %d, c: %c, n: %d", 1, 'p', n)
-	}
+	assertWalk(1, 'p', true, 2, tr, t)
+	// assertWalk(2, 'o', true, 3, tr, t)
 
 	t.Logf("base: %v", tr.base)
 	t.Logf("next: %v", tr.next)
 	t.Logf("check: %v", tr.check)
+}
+
+func assertWalk(s int, c rune, r bool, n int, tr *ThreeArrayTrie, t *testing.T) {
+	ar, an := tr.walk(s, c)
+	if ar != r {
+		t.Fatalf("failed s: %d, c: %c, r: %t", s, c, r)
+	}
+	if an != n {
+		t.Fatalf("failed s: %d, c: %c, r: %t", s, c, r)
+	}
 }
 
 func TestWalk(t *testing.T) {
