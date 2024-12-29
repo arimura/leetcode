d70fc7be11180e8364658b83bd10b4b55928a3ae
arimura <33759663+arimura@users.noreply.github.com>
2024-12-29 11:22:10 +0900

    Kick darts

--- double-array-trie/go/main.go
+++ double-array-trie/go/main.go
@@ -1,79 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-//Based on https://linux.thai.net/~thep/datrie/datrie.html
+	"github.com/arimura/leetcode/double-array-trie/darts"
+)
 
-var base []int
-var next []int
-var check []int
-var currnt int
-
-var ckey = []rune{
-	'a', //0
-	'c', //1
-	'd', //2
-	'e', //3
-	'g', //4
-	'i', //5
-	'l', //6
-	'o', //7
-	'p', //8
-	'r', //9
-	's', //10
-	'u', //11
-	'v', //12
-	'w', //13
-	'z', //14
-}
-
-var keyMap = map[rune]int{
-	'a': 0,
-	'c': 1,
-	'd': 2,
-	'e': 3,
-	'g': 4,
-	'i': 5,
-	'l': 6,
-	'o': 7,
-	'p': 8,
-	'r': 9,
-	's': 10,
-	'u': 11,
-	'v': 12,
-	'w': 13,
-	'z': 14,
-}
-
-var transition = [][]rune{
-	{},    //not used
-	{'p'}, //root node
-	{'r', 'o'},
-	{'o'},
-}
-
-func walk(s, c int) {
-	t := base[s] + c
-	if check[t] == s {
-		currnt = next[t]
-	} else {
-		panic(fmt.Sprintf("t: %d isn't in check\n", t))
+func main() {
+	d, err := darts.Import("darts.txt", "dats.libz", false)
+	if err == nil {
+		if d.ExactMatchSearch([]rune("テスト"), 0) {
+			fmt.Println("テスト is in dictionary")
+		}
 	}
-}
-
-func simpleInsert() {
-	//start from root node
-	//s = 0
-}
-
-// state: each state is a node in the trie and is represented by an integer
-// baseIndex:  For a trie node s, base[s] is the starting index within the next and check pool (to be explained later) for the row of the node s in the transition table.
-func relocate(s int, b int) {
-	for _, ck := range transition[s] {
-		c := keyMap[ck]
-		check[b+c] = s
-		next[b+c] = next[base[s]+c]
-		check[base[s]+c] = 0
-	}
-	base[s] = b
 }
