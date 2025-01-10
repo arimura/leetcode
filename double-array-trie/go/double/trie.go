a504eda056af55a10b53101468af8ce44c93e333
arimura <33759663+arimura@users.noreply.github.com>
2025-01-10 09:21:45 +0900

    Impl dat

--- double-array-trie/go/double/trie.go
+++ double-array-trie/go/double/trie.go
@@ -1,69 +1,32 @@
 package double
 
 type DoubleArrayTrie struct {
-	capacity int
-	base     []int
-	check    []int
+	base  []int
+	check []int
 }
 
 var keyMap = map[rune]int{
-	'a': 0,
-	'c': 1,
-	'd': 2,
-	'e': 3,
-	'g': 4,
-	'i': 5,
-	'l': 6,
-	'o': 7,
-	'p': 8,
-	'r': 9,
-	's': 10,
-	'u': 11,
-	'v': 12,
-	'w': 13,
-	'z': 14,
+	'a': 1,
+	'b': 2,
+	'c': 3,
+	'd': 4,
+	'e': 5,
+	'#': 6,
 }
 
-type transition struct {
-	next int
-	c    rune
-}
-
+// Double Array Trie based on https://www.slideshare.net/higashiyama/ss-8738479#2
 func New(c int) *DoubleArrayTrie {
 	t := &DoubleArrayTrie{
-		capacity: c,
-		base:     make([]int, c),
-		check:    make([]int, c),
+		base:  make([]int, c),
+		check: make([]int, c),
 	}
 	return t
 }
 
-// walk from state:s by character:c
-// return (success, next state)
-func (x *DoubleArrayTrie) walk(s int, c rune) (bool, int) {
-	t := x.base[s] + keyMap[c]
-	if x.check[t] == s {
+func (d *DoubleArrayTrie) walk(s int, r rune) (bool, int) {
+	t := d.base[s] + keyMap[r]
+	if d.check[t] == s {
 		return true, t
-	} else {
-		return false, 0
 	}
-}
-
-func (x *DoubleArrayTrie) available(s int, b int, c rune) bool {
-	if x.base[s] != 0 {
-		return false
-	}
-	if x.check[b+keyMap[c]] != 0 {
-		return false
-	}
-	return true
-}
-
-func (x *DoubleArrayTrie) insert(s int, b int, c rune, n int) bool {
-	if !x.available(s, b, c) {
-		return false
-	}
-	x.base[s] = b
-	x.check[x.base[s]+keyMap[c]] = s
-	return true
+	return false, 0
 }
