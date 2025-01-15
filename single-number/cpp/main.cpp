b5983b467c02b8a8f2b9ade96284cebb593d7b48
arimura <33759663+arimura@users.noreply.github.com>
2025-01-15 13:22:21 +0900

    Add template

--- single-number/cpp/main.cpp
+++ single-number/cpp/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
     std::cout << "hoge" << std::endl;
     return 0;
 }
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+    }
+};
