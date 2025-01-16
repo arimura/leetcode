f007544d47b20b28fef679faa34f1634a76361e2
arimura <33759663+arimura@users.noreply.github.com>
2025-01-16 09:30:13 +0900

    Add proto impl

--- single-number/cpp/main.cpp
+++ single-number/cpp/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
@@ -7,14 +8,24 @@ class Solution
 {
 public:
     int singleNumber(vector<int> &nums) {
-        return 0;
+        set<int> s;
+        for(int n : nums) {
+            if (s.find(n) != s.end()){
+                s.erase(n);
+            }else {
+                s.insert(n);
+            }
+        }
+
+        auto b = s.begin();
+        return *b;
     }
 };
 
 int main()
 {
     std::cout << "hoge" << std::endl;
-    vector<int> nums = {4, 1, 2, 1, 2};
+    vector<int> nums = {4, 3, 2, 1, 2};
     Solution solution;
     int result = solution.singleNumber(nums);
     std::cout << "The single number is: " << result << std::endl;
