d8fa0ae2e7dea87302c111388c08284d2a5cdf87
arimura <33759663+arimura@users.noreply.github.com>
2025-01-16 09:10:34 +0900

    Fix

--- single-number/cpp/main.cpp
+++ single-number/cpp/main.cpp
@@ -3,14 +3,21 @@
 
 using namespace std;
 
-int main() {
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums) {
+        return 0;
+    }
+};
+
+int main()
+{
     std::cout << "hoge" << std::endl;
+    vector<int> nums = {4, 1, 2, 1, 2};
+    Solution solution;
+    int result = solution.singleNumber(nums);
+    std::cout << "The single number is: " << result << std::endl;
     return 0;
 }
 
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        
-    }
-};
